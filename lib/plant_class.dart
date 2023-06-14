4697eec463fb7a61538bdb38244cba47d72e2235
Dilzo16 <chathuradilshanudawaththa@gmail.com>
2023-06-14 15:19:39 +0530

    fetch_data_from trefle api

---
+++ lib/plant_class.dart
@@ -0,0 +1,12 @@
+class Plant{
+  final int id;
+  final String commonName;
+  final String scientificName;
+  final String imageUrl;
+  
+
+  Plant({
+    required this.id,
+    required this.commonName,required this.scientificName,required this.imageUrl
+  });
+}
